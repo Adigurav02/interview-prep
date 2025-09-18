80fd0371d414c09c17d4eee948f4aad9923bd66d
Adigurav02 <adityagurav235@gmail.com>
2025-09-18 23:14:21 +0530

    feat: Add Vercel Web Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { Toaster } from "sonner";
 import type { Metadata } from "next";
 import { Mona_Sans } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react"; // <-- 1. IMPORT ADDED HERE
 
 import "./globals.css";
 
@@ -25,6 +26,7 @@ export default function RootLayout({
         {children}
 
         <Toaster />
+        <Analytics /> {/* <-- 2. COMPONENT ADDED HERE */}
       </body>
     </html>
   );
